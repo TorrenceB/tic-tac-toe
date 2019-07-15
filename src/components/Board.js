97dbc65a29cf3a8c10f248e281ef36b5d0711409
Torrence Brown <torrenceb90@gmail.com>
2019-07-15 15:14:17 -0700

    Modified Board Comp

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Square from './Square';
 
-export const Board = () => (
-    <div className="board-container">
-        <div className="row">
-            <div className="col-12">
-                <Square name={'Coconut'}/>
-                <Square name={'Banana'} /> 
-                <Square name={'Mandarin'}/>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12">
-                <Square />
-                <Square /> 
-                <Square />
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12">
-                <Square />
-                <Square /> 
-                <Square />
-            </div>
-        </div>
-    </div>
-)
+export default class Board extends Component {
 
-export default Board; 
+    state = {
+        X: '',
+        O: '',
+        values: []
+    }
+
+    handleClick = () => {
+        
+    }
+
+    render() {
+        return (
+            this.state.values.map( (value, index) => {
+                return <Square name={value} key={index} />
+            })
+        )
+    }
+}
+
