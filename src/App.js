cc337aac32ee3a366df9f500a28a1f938bc84449
Torrence Brown <torrenceb90@gmail.com>
2019-07-23 11:24:42 -0700

    Working on conditional

--- src/App.js
+++ src/App.js
@@ -8,43 +8,8 @@ export default class App extends Component {
 
 render() {
     return (
-      <div className="container">
-        {/* Row 1 */}
-        <div className="row">
-          <div className="square">
-            <h1 className="display-1 text-center">X</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">X</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">O</h1>
-          </div>
-        </div>
-        {/* Row 2 */}
-        <div className="row">
-          <div className="square">
-            <h1 className="display-1 text-center">O</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">X</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">X</h1>
-          </div>
-        </div>
-        {/* Row 3 */}
-        <div className="row">
-          <div className="square">
-            <h1 className="display-1 text-center">O</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">O</h1>
-          </div>
-          <div className="square">
-            <h1 className="display-1 text-center">X</h1>
-          </div>
-        </div>
+      <div className="container-fluid">
+        <Board />
         <Scoreboard />
       </div>
     );
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,24 +5,77 @@ import Square from './Square';
 export default class Board extends Component {
 
     state = {
-        value: '',
         values: Array(9).fill(null), 
+        value: '',
+        isPlayerX: true,
         gameFinished: false,
+        winner: null,
     }
 
+    gameFinishedHandler = () => {}
+
     handleClick = () => {
-        // let state = this.state;
-        // let value = state.value;
-        // let values = state.values;
-        // values.push(value);
-        // this.setState({values:values,value:'X'});
+        let state = this.state;
+        let turnCount = 0;
+        let value = state.value;
+        let values = [...state.values, ...value];
+        //Player 1 --> Even #'s
+        //Player 2 --> Odd #'s
+        if(turnCount/2 === 0){
+            //Player 1
+            //value = 'X'
+            this.setState( { values: values, value: 'X'} );
+        } else {
+            //Player 2
+            //value = 'O'
+            this.setState( {value: 'O'} );
+        }
     }
 
     render() {
+        let squares = this.state.values.map( (value, index) => {
+            return <Square value={value} key={index} onClick={this.handleClick} />
+        });
+
         return (
-            this.state.values.map( (value, index) => {
-                return <Square value={value} key={index} onClick={this.handleClick} />
-            })
+            <div className="container">
+            {/* Row 1 */}
+            <div className="row">
+              <div className="square">
+                {squares}
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">X</h1>
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">O</h1>
+              </div>
+            </div>
+            {/* Row 2 */}
+            <div className="row">
+              <div className="square">
+                <h1 className="display-1 text-center">O</h1>
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">X</h1>
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">X</h1>
+              </div>
+            </div>
+            {/* Row 3 */}
+            <div className="row">
+              <div className="square">
+                <h1 className="display-1 text-center">O</h1>
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">O</h1>
+              </div>
+              <div className="square">
+                <h1 className="display-1 text-center">X</h1>
+              </div>
+            </div>
+          </div>
         )
     }
 }
