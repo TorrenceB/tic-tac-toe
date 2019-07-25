1fa96f9e214f4336043a2ca9f56014d03260349c
Torrence Brown <torrenceb90@gmail.com>
2019-07-25 16:24:59 -0700

    Updated handleClick function

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,42 +7,40 @@ export default class Board extends Component {
     state = {
         values: Array(9).fill(null), 
         value: '',
-        isPlayerX: true,
+        isPlayerX: true, //Boolean to flip if PlayerO
         gameFinished: false,
         winner: null,
+        boardState: {},
     }
 
     gameFinishedHandler = () => {}
 
     handleClick = () => {
+        
         let state = this.state;
-        let turnCount = 0;
         let value = state.value;
-        let values = [...state.values, ...value];
+        let values = [...state.values];
         //Player 1 --> Even #'s
         //Player 2 --> Odd #'s
-        if(turnCount/2 === 0){
-            //Player 1
-            //value = 'X'
-            this.setState( { values: values, value: 'X'} );
-        } else {
-            //Player 2
-            //value = 'O'
-            this.setState( {value: 'O'} );
-        }
+        values[value] = 'X';
+        this.setState( { values: values } );
+        console.log(values);
     }
 
     render() {
-        let squares = this.state.values.map( (value, index) => {
-            return <Square value={value} key={index} onClick={this.handleClick} />
-        });
+        // let squares = this.state.values.map( (value, index) => {
+        //     return <Square
+        //             value={value}
+        //             key={index}
+        //             onClick={this.handleClick} />
+        // });
 
         return (
             <div className="container">
             {/* Row 1 */}
             <div className="row">
               <div className="square">
-                {squares}
+                <button className="test-btn" onClick={this.handleClick} >{this.state.value}</button>
               </div>
               <div className="square">
                 <h1 className="display-1 text-center">X</h1>
